f8bcbecd043762b47ec04c5b13bee5fb69ca2fe0
Basith P <basithpofficial@gmail.com>
2023-09-19 04:39:45 +0530

    add mui

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Box, CssBaseline } from "@mui/material";
+
+const Home = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Box>Home</Box>
+    </>
+  );
+};
+
+export default Home;
